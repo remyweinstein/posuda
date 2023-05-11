808638a44129bcd618f3acc841f74efeb3797580
remyweinstein <d.grebenyuk30@gmail.com>
2023-05-11 18:37:16 +1000

    fix version

--- app/php/version.php
+++ app/php/version.php
@@ -1,4 +1,4 @@
 <?php
-    define("APP_VERSION", "APP_238");
-    define("APP_VERSION_ANDROID", "238");
-    define("APP_VERSION_IOS", "238");
+    define("APP_VERSION", "APP_1");
+    define("APP_VERSION_ANDROID", "1");
+    define("APP_VERSION_IOS", "1");
