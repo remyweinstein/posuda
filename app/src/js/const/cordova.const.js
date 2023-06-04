ca7075a7fb74ea133c2eb6c47262983542d7a773
Remy Weinstein <d.grebenyuk30@gmail.com>
2023-06-05 08:59:58 +1000

    fix path to cordova

--- app/src/js/const/cordova.const.js
+++ app/src/js/const/cordova.const.js
@@ -1,5 +1,5 @@
 const DOMAIN = "https://bonus.xn--27-jlcptnghlu3g.xn--p1ai";
 
 let script = document.createElement('script');
-script.src = "../../../cordova.js";
+script.src = "cordova.js";
 document.head.append(script);
