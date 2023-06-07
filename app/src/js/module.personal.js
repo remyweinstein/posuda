0d8973850fd221136a9109ce3224a0f8354a90cb
Remy Weinstein <d.grebenyuk30@gmail.com>
2023-06-07 21:32:27 +1000

    fixes

--- app/src/js/module.personal.js
+++ app/src/js/module.personal.js
@@ -123,8 +123,8 @@ function drawPersonal(personal) {
         C("#personal_phone").text(`+${a[0]} (${a[1]}${a[2]}${a[3]}) ${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}-${a[9]}${a[10]}`);
     }
 
-    if (data.email) {
-        C("#personal_email").text(data.email);
+    if (personal.email) {
+        C("#personal_email").text(personal.email);
     } else {
         C('input[name^="enableNotifyEmail"]').el.disabled = "disabled";
         C("#personal_email").text('-');
