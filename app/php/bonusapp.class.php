bbccac007ae8d36dee9bcc84019500f143c187f6
Remy Weinstein <d.grebenyuk30@gmail.com>
2023-07-05 14:42:18 +1000

    fix

--- app/php/bonusapp.class.php
+++ app/php/bonusapp.class.php
@@ -467,7 +467,7 @@ class BonusApp
     {
         $result = $this->initPDO();
         $LMX = $this->getLMX();
-        print_r($LMX->chargeOnRegisterBonus('79142030924'));
+        print_r($LMX->chargeOnRegisterBonus('79084470358'));
     }
 
     private function api($rawRequestData)
